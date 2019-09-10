65aa92da52ed5842a996ae884f75e46ccf3a311d
Sally Beckwith <sallypbeckwith@gmail.com>
2019-09-10 16:59:21 -0400

    redux store keeps track of items added to cart - but still needs work

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,8 @@
 // these mappings make it so you don't accidentally mistype the real action name
 
 const CartActionTypes = {
-  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM'
 }
 
 export default CartActionTypes;
