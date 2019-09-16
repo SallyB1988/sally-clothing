417a7dbea10e297fc89c48af960ae1a01efefc81
Sally Beckwith <sallypbeckwith@gmail.com>
2019-09-16 14:13:25 -0400

    finished section 18

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,34 @@
 import ShopActionTypes from './shop.types'
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+
+// THUNKS are action creators that return a function that gets a dispatch
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+})
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 })
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    // Redux will dispatch the fetchCollectionStart which changes the reducer state to be isFetching: true, then
+    // it will begin the async request for collectionRef. This will complete with either success (fetchCollectionsSuccess)
+    //  or failure (fetchCollectionsFailure)
+    dispatch(fetchCollectionsStart());
+
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      dispatch(fetchCollectionsSuccess(collectionsMap));
+    }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+  }
+}
